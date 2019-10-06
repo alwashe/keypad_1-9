3d49735394dc893ad9b6231575bb198484970c5a
alwashe <15383159+alwashe@users.noreply.github.com>
2019-10-06 16:24:44 +0200

    Update pin.py

--- pin.py
+++ pin.py
@@ -1,41 +1,42 @@
 import paho.mqtt.client as mqtt
 import time
 
-pin = []
-s = ''
-first=True
-second=False
-domainTopic = "test12367"
-keypadTopic = "keypad"
-pinTopic = "pin"
+pin = []                                                # List to save the pin
+s = ''                                                  # seperator for the join command 1,2,3,4 -> 1234
+first=True                                              # first run            
+second=False                                            # folowing runs
+domainTopic = "test12367"                               # name for the first topic              domainTopic/####
+keypadTopic = "keypad"                                  # topicname for every keypress          domainTopic/keypadTopic
+pinTopic = "pin"                                        # topicname for every pin               domainTopic/pinTopic
+maxElapsedTime = 10
 
-def on_connect(client, userdata, flags, rc):
-    client.subscribe(domainTopic +"/" +keypadTopic)
+def on_connect(client, userdata, flags, rc):            # callback for mqtt connection
+    client.subscribe(domainTopic +"/" +keypadTopic)     # mqtt topic subscribe for keypad press
 
-def on_message(client, userdata, msg):
-    global first
-    global second
-    global start_time
-    if first:
-        start_time = time.time()
-        first = False
-        second = True
-    elif second:
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        elapsed_time = int(elapsed_time)
-        start_time = time.time()
-    if elapsed_time >= 10:
-        start_time = time.time()
-        pin.clear()
-    msg.payload = msg.payload.decode("utf-8")
-    if msg.payload in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        pin.append(msg.payload)
-        if int(s.join((pin)))  > 999:
-            client.publish(domainTopic +"/" +pinTopic, s.join(map(str, pin)))
-            del pin[0]
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("test.mosquitto.org", 1883, 60)
-client.loop_forever()
+def on_message(client, userdata, msg):                  # callback when receiving message
+    global first                                        # var is eddited in module
+    global second                                       # var is eddited in module
+    global start_time                                   # var is eddited in module
+    if first:                                           # first run, to save time for first keypress
+        start_time = time.time()                        # save start time for elapsed time calculation between the keypresses
+        first = False                                   # disable first run
+        second = True                                   # enable second if for all following runs
+    elif second:                                        # second and following keypress
+        end_time = time.time()                          # save end time for elapsed time calculation between the keypresses
+        elapsed_time = end_time - start_time            # elapsed time calculation
+        elapsed_time = int(elapsed_time)                # convert elapsed time calculation to int
+        start_time = time.time()                        # restart timer
+    if elapsed_time >= maxElapsedTime:                  # reset pin after the definded elapsed time
+        pin.clear()                                     # clear pin list
+        start_time = time.time()                        # restart timer after pin reset
+    msg.payload = msg.payload.decode("utf-8")                                       # convert 8-Byte format
+    if msg.payload in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:                # accept only numbers between 1-9 (because 0 is send after every press)
+        pin.append(msg.payload)                                                     # add number to pin list
+        if int(s.join((pin)))  > 999:                                               # if when number has 4 digits
+            client.publish(domainTopic +"/" +pinTopic, s.join(map(str, pin)))       # send pin to pin topic
+            del pin[0]                                                              # del first entry of list to end new last digit in next run
+client = mqtt.Client()                                                              # instance
+client.on_connect = on_connect                                                      # 
+client.on_message = on_message                                                      #
+client.connect("test.mosquitto.org", 1883, 60)                                      # connect to server
+client.loop_forever()                                                               # loop
